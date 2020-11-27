280f18d9722034866d1640384dd8f57f00bf79b9
Stephan van Stekelenburg <stephan@hayman.io>
2020-11-27 22:51:10 +0100

    content: remove podcast page
    .. since I have none..

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,10 +19,6 @@ module.exports = {
         link: `/blog`,
       },
       {
-        name: `Podcast`,
-        link: `/podcast`,
-      },
-      {
         name: `Portfolio`,
         link: `/portfolio`,
       },
@@ -71,27 +67,6 @@ module.exports = {
     `@arshad/gatsby-theme-page-core`,
     `@arshad/gatsby-theme-portfolio-core`,
     `@arshad/gatsby-theme-photo-core`,
-    {
-      resolve: `@arshad/gatsby-theme-podcast-core`,
-      options: {
-        feedUrl: `https://feeds.megaphone.fm/travelgenius`,
-        podcast: {
-          name: `Travel Genuis`,
-          description: `Eligendi nisi nobis nisi voluptate. Corporis deserunt provident hic numquam. Veritatis vero necessitatibus adipisci cumque voluptate rerum at.`,
-          image: `assets/images/podcast.jpg`,
-          social: [
-            {
-              name: `Apple Podcast`,
-              url: `https://itunes.apple.com`,
-            },
-            {
-              name: `Google Podcast`,
-              url: `https://podcasts.google.com`,
-            },
-          ],
-        },
-      },
-    },
     `@arshad/gatsby-theme-phoenix`,
     {
       resolve: `gatsby-plugin-postcss`,
